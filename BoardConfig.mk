1b6c2be1237bd356bf9dc721ca741f7ec1ae2f81
Hashcode <hashcode0f@gmail.com>
2016-12-15 16:52:11 -0800

    Revert "TEMP: selinux back to permissive mode for now"
    This reverts commit 846cf8308719557e7c2c0028b63c27a567cdff4d.
    
    Change-Id: I7f6f3641bb69b254adba2483e2757a777f64af8c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := apollo_defconfig
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 mdss_mdp.panel=0:qcom,mdss_dsi_jdi_dualmipi0_video:1:qcom,mdss_dsi_jdi_dualmipi1_video: androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 mdss_mdp.panel=0:qcom,mdss_dsi_jdi_dualmipi0_video:1:qcom,mdss_dsi_jdi_dualmipi1_video:
 
 # inherit from common hdx
 -include device/amazon/hdx-common/BoardConfigCommon.mk
